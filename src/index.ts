4475385bfe9b28f4193f3ac1bf974196b7d82da8
kamilkulik <kulikkamil@gmail.com>
2021-04-06 16:09:21 +0200

    add user model and configure sequalize

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import { DiContainer } from './core/di-container';
 import { Sequelize } from 'sequelize-typescript';
+import { User } from './models';
 
 (() => {
   const dependencies = DiContainer.getInstance();
-  const sequelize = new Sequelize('database', 'username', 'password', {
+  const sequelize = new Sequelize('database', (process.env.USER as unknown) as string, '', {
     dialect: 'postgres',
-    host: '0.0.0.0',
-    logging: true,
+    host: '127.0.0.1',
+    logging: console.log,
     port: 5432,
   });
+  sequelize.addModels([User]);
   sequelize.sync({
     force: true,
   });
-  sequelize.getRepository('modelClass');
+  // sequelize.getRepository('modelClass');
 })();
