ce100b5548f37278f7eabcbd8af789601c4b6464
kamilkulikTeck <kulikkamil@gmail.com>
2021-07-13 14:30:19 +0200

    add try catch to controllers

--- src/controllers/comments-controller.ts
+++ src/controllers/comments-controller.ts
@@ -8,22 +8,25 @@ export class CommentsController {
   constructor(
     private commentsTableService: CommentsTableServiceInterface,
     private apiService: ApiResponseServiceInterface
-  ) {
-  }
+  ) {}
 
   public createComment = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    const comment = await this.commentsTableService.create(new CreateCommentDTO(req.body));
+    try {
+      const comment = await this.commentsTableService.create(new CreateCommentDTO(req.body));
 
-    if (!comment) return next(new Error('error creating comment'));
-
-    res.send(this.apiService.successResponse<Comment>('success', comment));
+      res.send(this.apiService.successResponse<Comment>('success', comment));
+    } catch (error) {
+      next(error);
+    }
   };
 
   public getComment = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    const comment = await this.commentsTableService.findOne(Number(req.params.id));
+    try {
+      const comment = await this.commentsTableService.findOne(Number(req.params.id));
 
-    if (!comment) return next(new Error('no comment!'));
-
-    res.send(this.apiService.successResponse<Comment>('success', comment));
+      res.send(this.apiService.successResponse<Comment>('success', comment as Comment));
+    } catch (error) {
+      next(error);
+    }
   };
 }
