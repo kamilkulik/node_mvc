2762fdc6aa8ac342189096aa0f9ab8f77028d1a4
kamilkulikTeck <kulikkamil@gmail.com>
2021-07-13 14:59:43 +0200

    add password validator to CreateUserDTO

--- src/dtos/users-dto.ts
+++ src/dtos/users-dto.ts
@@ -6,6 +6,19 @@ export class CreateUserDTO {
 
   constructor({ email, password }: CreateUserProperties) {
     if (!validator.isEmail(email)) throw new Error('field: "email": must be an email!');
+    if (
+      !validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+        minUppercase: 1,
+      })
+    ) {
+      throw new Error(
+        'field: "password": must be at least 8 characters long, have at least 1 lowercase, 1 uppercase letter, 1 number and include at least 1 symbol'
+      );
+    }
 
     this.email = email;
     this.password = password;
