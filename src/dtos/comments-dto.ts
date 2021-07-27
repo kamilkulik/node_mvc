ecb187a3663ca49d1fbb6d3625cea34c411ec58f
kamilkulikTeck <kulikkamil@gmail.com>
2021-07-27 13:38:15 +0200

    add CommentResponseDTO

--- src/dtos/comments-dto.ts
+++ src/dtos/comments-dto.ts
@@ -3,9 +3,42 @@ export class CreateCommentDTO {
   public readonly content: string;
   public readonly userId: number;
 
-  constructor(d: { blogPostId: number; content: string; userId: number }) {
-    this.blogPostId = d.blogPostId;
-    this.content = d.content;
-    this.userId = d.userId;
+  constructor({ blogPostId, content, userId }: CreateCommentProperties) {
+    this.blogPostId = blogPostId;
+    this.content = content;
+    this.userId = userId;
   }
 }
+
+type CreateCommentProperties = {
+  blogPostId: number;
+  content: string;
+  userId: number;
+};
+
+export class CommentResponseDTO {
+  public readonly blogPostId: number;
+  public readonly content: string;
+  public readonly createdAt: Date;
+  public readonly id: number;
+  public readonly updatedAt: Date;
+  public readonly userId: number;
+
+  constructor({ blogPostId, content, createdAt, id, updatedAt, userId }: CommentResponseProperties) {
+    this.blogPostId = blogPostId;
+    this.content = content;
+    this.createdAt = createdAt;
+    this.id = id;
+    this.updatedAt = updatedAt;
+    this.userId = userId;
+  }
+}
+
+export type CommentResponseProperties = {
+  blogPostId: number;
+  content: string;
+  createdAt: Date;
+  id: number;
+  updatedAt: Date;
+  userId: number;
+};
