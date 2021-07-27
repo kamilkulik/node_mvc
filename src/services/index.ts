5301eb6753c157fb3a70b67e65dfa14e09822490
kamilkulikTeck <kulikkamil@gmail.com>
2021-07-27 17:04:54 +0200

    export MapperService

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,7 @@
 export * from './api-response-service';
 export * from './blog-posts-table-service';
 export * from './comments-table-service';
+export * from './mapper-service';
 export * from './placki-service';
 export * from './postgres-provider-service';
 export * from './users-table-service';
